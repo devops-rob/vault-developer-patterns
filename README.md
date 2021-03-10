2ff8c11af7f89616257fdc6bcc2c6bac98b715f3
Robert Barnes (DevOps Rob) <rbarnes@hashicorp.com>
2021-03-10 12:12:01 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vault-developer-patterns
---
+++ scenario1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+##########################################################
+# Scenario 1
+# Encrypting application data using Vault's API
+##########################################################
+
+TOKEN="root"
+VAULT_ADDR="http://127.0.0.1:8200"
